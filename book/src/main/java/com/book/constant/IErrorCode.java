0438c2bb491930eaf32f34565c0483ceebfd8b5b
lunacia <xpwanghy@hotmail.com>
2020-06-10 15:09:40 +0800

    删除重复文件

--- book/src/main/java/com/book/constant/IErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.book.constant;
-
-public interface IErrorCode {
-
-    int getCode();
-
-    String getMsg();
-
-}
--- book/src/main/java/com/book/mapper/RememberMapper.java
+++
@@ -1,8 +0,0 @@
-package com.book.mapper;
-
-import com.book.model.Remember;
-import tk.mybatis.mapper.common.BaseMapper;
-
-
-public interface RememberMapper extends BaseMapper<Remember> {
-}
